e3d469990e1f8a6f227baf5de0dc803027a3049a
ProgramC <krygrish@yandex.ru>
2022-09-23 16:12:18 +0300

    Init commit

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    // protected $model = Comment::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    
+    public function definition()
+    {
+        return [
+            'subject' => $this->faker->sentence('3'),
+            'body' => $this->faker->paragraph('3', false),
+        ];
+    }
+}
