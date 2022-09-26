42c2725e2d5ca67cf53b3fb4c6cec08db309dd5f
ProgramC <krygrish@yandex.ru>
2022-09-26 18:38:34 +0300

    Add active link class in navbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        $this->activeLinks();
+    }
+
+    public function activeLinks()
+    {
+        View::composer('layouts.app', function($view) {
+            $view->with('mainLink', request()->is('/') ? 'menu-link__active' : '');
+            $view->with('articleLink', (request()->is('articles') or request()->is('articles/*')) ? 'menu-link__active' : '');
+        });
     }
 }
