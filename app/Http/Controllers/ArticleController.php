3ddd430a45d9bad1f1aee73b33e7ab0d80bbd34f
ProgramC <krygrish@yandex.ru>
2022-09-27 11:53:50 +0300

    Add tag route and tag template

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
+use App\Models\Tag;
 
 class ArticleController extends Controller
 {
-    function index()
+    public function index()
     {
         $articles = Article::allPaginate(10);
         return view('app.article.index', compact('articles'));
     }
 
-    function show($slug)
+    public function show($slug)
     {
         $article = Article::findBySlug($slug);
         return view('app.article.show', compact('article'));
+    }
+
+    public function allByTag(Tag $tag)
+    {
+        $articles = $tag->articles()->findByTag();
+        return view('app.article.byTag', compact('articles'));
     }
 }
