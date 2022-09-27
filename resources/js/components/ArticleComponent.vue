88f4c7d426bea392719926a49b4e3bfbb03f72b7
ProgramC <krygrish@yandex.ru>
2022-09-27 16:36:03 +0300

    Add experiment with Vue Article Component with Vuex store

---
+++ resources/js/components/ArticleComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <h1>Hello {{fullname}} - {{name}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('Component mounted.')
+  },
+  computed: {
+    fullname() {
+      return this.$store.getters.getFullName;
+    },
+    name() {
+      return this.$store.state.firstname;
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
